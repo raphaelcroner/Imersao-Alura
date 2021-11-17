e83674c971c3c545f517fd41a60463966e1c5dfa
Raphael Alves Croner <42879853+raphaelcroner@users.noreply.github.com>
2021-11-17 17:39:47 -0300

    aula 1

---
+++ media.js
@@ -0,0 +1,10 @@
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 7
+var notaTerceiroBimstre = 4
+var notaQuartoBimestre = 2
+
+var notafinal = notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimstre + notaQuartoBimestre
+
+var media = notafinal / 4
+
+console.log("A nota final do aluno foi " + notafinal + " e sua média foi " + media.toFixed(1))
